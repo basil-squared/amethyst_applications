704b8c58727af9adfc4f6e621187ad65ef1244a0
Juniper <basil.squared1@gmail.com>
2026-01-21 21:13:33 -0600

    (wip) work on ui n shiz

---
+++ common/src/main/kotlin/basilsquared/amethyst_applications/blockentities/ChargerBlockEntity.kt
@@ -0,0 +1,4 @@
+package basilsquared.amethyst_applications.blockentities
+
+class ChargerBlockEntity {
+}
---
+++ common/src/main/kotlin/basilsquared/amethyst_applications/botariumreqitems/Charger.kt
@@ -0,0 +1,29 @@
+package basilsquared.amethyst_applications.botariumreqitems
+
+import net.minecraft.core.BlockPos
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.BlockHitResult
+import java.util.Properties
+
+class Charger(properties: BlockBehaviour.Properties): Block(properties) {
+    @Deprecated("Deprecated in Java")
+    override fun use(
+        state: BlockState,
+        level: Level,
+        pos: BlockPos,
+        player: Player,
+        hand: InteractionHand,
+        hit: BlockHitResult
+    ): InteractionResult {
+        if (!level.isClientSide) {
+            val blockEntity = level.getBlockEntity(pos)
+            if (blockEntity is )
+        }
+    }
+}
